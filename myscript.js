3cc7bab80fdbc6dc9555932594b1e087fb0767dc
frederickbeaud <fredbeaud.fb@gmail.com>
2024-08-03 11:23:46 +0100

    premier push pour home page

---
+++ myscript.js
@@ -0,0 +1,22 @@
+/*anime({
+    targets: '.gauche',
+    translateX: 250
+  });*/
+  anime({
+    targets: '.herro',
+    translateX: 250,
+    direction: 'reverse',
+    easing: 'easeInOutSine'
+  });
+  anime({
+    targets: '.droite',
+    translateX: 250,
+    direction: 'alternate',
+    easing: 'easeInOutSine'
+  });
+  anime({
+    targets: '.gauche',
+    translateX: 250,
+    direction: 'reverse',
+    easing: 'easeInOutSine'
+  });
